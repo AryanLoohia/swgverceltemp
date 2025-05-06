21e10d7feed75c651b5a5b68f11d1385d1b030e2
Aryan Lohia <aryanlohia2005@gmail.com>
2025-05-06 22:42:20 +0530

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import FAQ from './FAQ'
+import Hblog from './Hblog'
+import HeroNew from './Hero/HeroNew'
+import HEvents from './HEvents'
+import styled, { StyledComponent } from 'styled-components'
+import HIntro from './HIntro'
+import Footer from 'components/Footer/Footer'
+import ScrollToTopH from 'components/ScrollToTopH'
+
+const HomeTag = styled.section`
+  position: relative;
+  margin-top: 8vh;
+  min-height: 92vh;
+  background: #fff;
+`
+const Home = () => {
+  return (
+    <HomeTag>
+        {/* <Hero/> */}
+        <HeroNew/>
+        {/* <Intro3d/> */}
+        <HIntro/>
+        {/* <HAbout/> */}
+        {/* <Intro/> */}
+        {/* <HGallery/> */}
+        <ScrollToTopH/>
+        <HEvents/>
+        <Hblog/>
+        <FAQ/>
+        {/* <Footer/> */}
+        <Footer/>
+    
+    </HomeTag>
+  )
+}
+
+export default Home
